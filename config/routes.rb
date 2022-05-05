dbdb475c6da4485f15a8dc3f4a5c271e7e545da4
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-05-05 00:09:16 +0000

    add about us route/view/styling

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:index]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
