1a7ef4a2bf085b5df6fc450c658d212c814c6acc
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-05-05 18:25:59 +0000

    create categories controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
   get 'static_pages/help'
   get 'static_pages/faq'
   get 'static_pages/about'
@@ -20,6 +25,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
