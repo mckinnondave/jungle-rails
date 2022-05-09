470bf33bb027141238f7d8225cee54346d9c86e0
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-05-09 19:53:20 +0000

    add validation specs

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+    before do
+      @category = Category.new({:name => "Plant Seeds"})
+      @product = Product.new({:name => "Aspen Tree Seeds", :price_cents => "200", :quantity => 3000, :category_id => @category.id})
+    end
+
+    describe "Validations" do
+
+      it "Should save a new product if all 4 fields are set" do
+        @product.save
+        expect(@product).to be_present
+      end
+
+      it "Should not save if name is not set" do
+        @product.name = nil
+        @product.save
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it "Should not save if price is not set" do
+        @product.price_cents = nil
+        @product.save
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it "Should not save if quantity is not set" do
+        @product.quantity = nil
+        @product.save
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      end
+
+      it "Should not save if category is not set" do
+        @product.quantity = nil
+        @product.save
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+    end
+end
